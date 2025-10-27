fcbb8b8cd7716513bf2a46c15b891dc74f155feb
leskbn <leskbn011@naver.com>
2025-10-27 13:43:10 +0900

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-from dm_control import suite
-
-env = suite.load("cartpole", "swingup")  # MuJoCo 물리
-ts = env.reset()
-for _ in range(1000):
-    action = env.action_spec().generate_value()
-    ts = env.step(action)
